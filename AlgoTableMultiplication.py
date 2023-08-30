27623c925cc8cce9498c446b5ccf41f7109794fc
mosheyah <dmosheyah@gmail.com>
2023-08-30 03:23:29 +0200

    LEs Akgorithme de fondamentaux de python

--- AlgoTableMultiplication.py
+++ AlgoTableMultiplication.py
@@ -1,4 +1,10 @@
 n = int(input("La table de multiplication par n : "))
 for i in range(13) :
   print(i,"X",n,"=",i*n)
-  
+# Algo table de multiplication avec la boucle while
+#  Soit la table de multiplication par K het que K est un nombre entier
+k = int(input("La table de multiplication par K : "))
+i = 0
+while i = 13  :# mon clavier ne possede pas les signes d'inegalité: La vraie methode est que tant que i est iferieur à 13
+  print(i,"X",k,"=",i*k)
+  i = i+1
